2443b442c14f7db16a1c97ae4ff76bd7f69c9454
LINCHENMUM <linchen10100@gmail.com>
2017-09-27 10:11:31 -0500

    Initial commit

---
+++ ETBOSSWindowsClient/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ETBOSSWindowsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ETBOSSService.ETBOSSServiceClient client = new ETBOSSService.ETBOSSServiceClient("NetTcpBinding_IETBOSSService");
+            label1.Text = client.GetCustomerName(textBox1.Text);
+        }
+    }
+}
